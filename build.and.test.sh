b4933d951eb809d9ad5bc0424bcb7b98dc34d281
jsalerno <julian@practica.com.au>
2025-09-26 18:26:51 +0200

    more graceful, but less reliable stop method; still hit it hard at the end to be sure;

--- build.and.test.sh
+++ build.and.test.sh
@@ -3,6 +3,7 @@ cd browse
 mvn spring-boot:run &
 cd ../manage
 mvn clean install
-ps -ef | grep java | grep spring-boot:run
-ps aux | grep java | grep spring-boot:run | awk {'print $2'} | xargs kill -9
+cd ../browse
+mvn spring-boot:stop
+ps aux | grep java | grep au.com.practica.src.challenge.BrowseApplication | awk {'print $2'} | xargs kill -9
 cd ../
