244f31557300fa9010134933b2b190f4a21348a0
jsalerno <julian@practica.com.au>
2025-09-28 19:41:07 +0200

    cleaner execution during test

--- common/src/main/java/au/com/practica/src/challenge/NoOpApplication.java
+++ common/src/main/java/au/com/practica/src/challenge/NoOpApplication.java
@@ -5,6 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NoOpApplication {
 	public static void main(String... args) {
-		throw new RuntimeException();
+		/* no-op */
 	}
 }
